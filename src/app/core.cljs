76210b812ad0400d0635cdaf58851324b52155ab
dan sutton <dan@dpsutton.com>
2019-11-19 17:21:13 -0600

    add some heat

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,13 +3,16 @@
    [reagent.core :as r]))
 
 (defn component [on-change value]
-  [:input {:on-change on-change :value value}])
+  [:input {:on-change on-change :value value
+           :style {:width "600px"
+                   :height "30px"}}])
 
 (defn app []
   (let [state (r/atom "")]
     (fn []
-      [component #(let [value (.. % -target -value)]
-                    (reset! state value))
+      [component #(let [heat (mapv (fn [x] (/ x 3.234234234234)) (range 1e4))
+                        value (.. % -target -value)]
+                    (js/setTimeout (fn [] (reset! state value)) 0))
        @state])))
 
 (defn ^:dev/after-load start
